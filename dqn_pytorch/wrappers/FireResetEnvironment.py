fca503a954bc3711c851b56be3d4866a93d4fe59
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-18 15:23:16 +0200

    adds fire reset environment

---
+++ dqn_pytorch/wrappers/FireResetEnvironment.py
@@ -0,0 +1,18 @@
+import gymnasium
+
+
+class FireResetEnvironment(gymnasium.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        assert env.unwrapped.get_action_meanings()[1] == "FIRE"
+        assert len(env.unwrapped.get_action_meanings()) >= 3
+
+    def reset(self, **kwargs):
+        self.env.reset(**kwargs)
+        obs, _, terminated, truncated, info = self.env.step(1)
+        if terminated or truncated:
+            self.env.reset(**kwargs)
+        obs, _, terminated, truncated, info = self.env.step(2)
+        if terminated or truncated:
+            self.env.reset(**kwargs)
+        return obs, info
---
+++ dqn_pytorch/wrappers/__init__.py
@@ -0,0 +1,1 @@
+from .FireResetEnvironment import FireResetEnvironment
